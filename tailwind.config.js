bc4079acc6792f47413f3d5168ecb533aad4a2ac
kun0620 <41094738+kun0620@users.noreply.github.com>
2025-09-25 10:03:02 +0700

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    safelist: [
+  { pattern: /(from|to|via)-(green|emerald|orange|amber)-(100|200|300|400|500|600|700)/ },
+],
   },
   plugins: [],
 };
